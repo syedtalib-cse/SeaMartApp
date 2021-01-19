44f9681ca89d63657eb6823a9b1990c9f8dc9da7
Md Taj Hassan <tajhassan0355@gmail.com>
2021-01-19 14:44:38 +0530

    Initial Commit

---
+++ SeaMartApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeaMartApp
+//
+//  Created by mehtab alam on 19/01/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
